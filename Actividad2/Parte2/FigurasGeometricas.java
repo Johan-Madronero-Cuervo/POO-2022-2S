a59a0dbd8eb0507180bf6e48483b5c69dbe2776a
Johan Madroñero Cuervo <92934320+Johan-Madronero-Cuervo@users.noreply.github.com>
2022-09-01 20:42:40 -0500

    Rombo y Trapecio agregados

--- Actividad2/Parte2/FigurasGeometricas.java
+++ Actividad2/Parte2/FigurasGeometricas.java
@@ -10,6 +10,8 @@ public class FigurasGeometricas
 	Rectangulo figura2 = new Rectangulo(1.0,2.0);
 	Cuadrado figura3 = new Cuadrado(3.0);
 	TrianguloRectangulo figura4 = new TrianguloRectangulo(3.0,5.0);
+	Rombo figura5 = new Rombo(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+	Trapecio figura6 = new Trapecio(1.0, 2.0, 3.0, 4.0, 5.0);
 	System.out.println("El área del círculo es = " + figura1.calcularArea());
 	System.out.println("El perímetro del círculo es = " + figura1.calcularPerimetro());
 	System.out.println();
@@ -22,7 +24,11 @@ public class FigurasGeometricas
 	System.out.println();
 	System.out.println("El área del triángulo es = " + figura4.calcularArea());
 	System.out.println("El perímetro del triángulo es = " + figura4.calcularPerimetro());
-	figura4.calcularTipodeTriangulo();	
+	figura4.calcularTipodeTriangulo();
+	System.out.println("El perimetro del rombo es = " + figura5.calcularPerimetro());
+	System.out.println("El area del rombo es = " + figura5.calcularArea());
+	System.out.println("El perimetro del trapecio es = " + figura6.calcularPerimetro());
+	System.out.println("El area del trapecio es = " + figura6.calcularArea());
 	}
 }
 
@@ -114,5 +120,55 @@ class TrianguloRectangulo extends FiguraGeometrica {
 		else{
 			System.out.println("Isosceles");
 		}
+	}
+}
+
+class Rombo extends FiguraGeometrica {
+	Double lado1;
+	Double lado2;
+	Double lado3;
+	Double lado4;
+	Double diagonalMenor;
+	Double diagonalMayor;
+
+		public Rombo(Double l1, Double l2, Double l3, Double l4, Double dMen, Double dMay){
+		lado1 = l1;
+		lado2 = l2;
+		lado3 = l3;
+		lado4 = l4;
+		diagonalMenor = dMen;
+		diagonalMayor = dMay;
+	}
+	
+	public Double calcularArea(){
+		return ((diagonalMayor*diagonalMenor)/2);
+	}
+
+	public Double calcularPerimetro(){
+		return (lado1 + lado2 + lado3 + lado4);
+	}
+}
+
+class Trapecio extends FiguraGeometrica {
+	Double baseMenor;
+	Double baseMayor;
+	Double lado1;
+	Double lado2;
+	Double altura;
+
+	public Trapecio(Double bMen, Double bMay, Double l1, Double l2, Double h){
+		baseMenor = bMen;
+		baseMayor = bMay;
+		lado1 = l1;
+		lado2 = l2;
+		altura = h;
+	}
+	
+	public Double calcularArea(){
+		return (((baseMenor + baseMayor)*altura)/2);
+	}
+
+	public Double calcularPerimetro(){
+		return (baseMenor + baseMayor + lado1 + lado2);
 	}
 }
